9c006f4246c8fe11d4708657cb03328fc1474fca
ElundC <nlemarchal@gmail.com>
2022-03-15 19:47:09 +0100

    lvel generation setup

--- Assets/Scripts/SpawnObjects.cs
+++ Assets/Scripts/SpawnObjects.cs
@@ -9,6 +9,53 @@ public class SpawnObjects : MonoBehaviour
 
     void Start()
     {
+        /*for(int x = 0; x < tiles.Count ; x++)             enleve tout les tiles avec le bon tag; 
+        {
+            if (tiles[x].CompareTag("Grey"))
+            {
+                tiles.Remove(tiles[x]);
+                x--;                                        si on enleve x-- une tile sur deux sera pas enlevé;
+            }
+    
+        }
+        
+        int countList = tiles.Count;                        duplique toute les tiles du bon tag; 
+
+        for(int x = 0; x < countList ; x++)
+        {
+            if (tiles[x].CompareTag("Snow"))
+            {
+                tiles.Add(tiles[x]);
+                x++;                                        avec x++ un sur deux de dupliquer;
+            }
+    
+        }
+        for(int x = 0; x < tiles.Count; x++)                enleve les tiles avec le layer 7 soit block
+        {
+            if(tiles[x].layer == 7)
+            {
+                tiles.Remove(tiles[x]);
+                x++;                                        sans rien enleve 1/2 avec x-- tout, avec x++ 1/3;
+            }
+        }
+        
+        int countList = tiles.Count;                       duplique les tiles avec le layer correspondant
+
+        for(int x = 0; x < tiles.Count; x++){
+
+            if(tiles[x].layer == 7)                        
+            {
+                tiles.Add(tiles[x]);                       !!!!! ne pas enlever x++ sinon crash car ajoute indefiniment des tiles dans la list
+                x += 3;                                    x++ = toute copier / x +=2 = 1/2 copier / x +=3 = 1/3:
+            }
+        }*/
+        
+        GenerateLevel();
+        
+    }
+
+        void GenerateLevel()
+    {
         if( transform.position.z == 0)
         {
             waitTime = 0;
@@ -29,7 +76,6 @@ public class SpawnObjects : MonoBehaviour
 
         StartCoroutine(WaitThenGenerate(waitTime));
     }
-  
     private IEnumerator WaitThenGenerate ( float waitTime)
     {
         yield return new WaitForSeconds (waitTime);
@@ -37,4 +83,4 @@ public class SpawnObjects : MonoBehaviour
         Instantiate(tiles[rand], transform.position, Quaternion.identity);
     }
 
-}
+}   
