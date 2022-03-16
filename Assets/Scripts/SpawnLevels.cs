5587df993f956e9a34cabb73d9a42d239cbe5f91
ElundC <nlemarchal@gmail.com>
2022-03-16 16:51:27 +0100

    rule levle generation made

--- Assets/Scripts/SpawnLevels.cs
+++ Assets/Scripts/SpawnLevels.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class SpawnLevels : MonoBehaviour
 {
     public List<GameObject> levels;
-    public int rand = 1; // Random.Range(0,4);
+
+    public RandomRule RuleNumber;
+    
     void Start()
     {
+        RuleNumber.value = Random.Range(0,13);
 
         int randlevel = Random.Range(0, levels.Count);
         Instantiate(levels[randlevel], transform.position, Quaternion.identity);
-    
     }
 }
--- Assets/Scripts/SpawnObjects.cs
+++ Assets/Scripts/SpawnObjects.cs
@@ -7,6 +7,8 @@ public class SpawnObjects : MonoBehaviour
     public List<GameObject> tiles;
 
     public GameObject SpawnerLevel;
+
+    public RandomRule RuleNumber;
     void Start()
     {
 
@@ -61,13 +63,10 @@ public class SpawnObjects : MonoBehaviour
     {
         int rand = Random.Range(0, tiles.Count);
 
-        int rulenum = SpawnerLevel.GetComponent<SpawnLevels>().rand;
-
-        
-
+        int rulenum = RuleNumber.value;
 
         if(transform.parent.CompareTag("Level")) {
-           rand = ChooseRule(rulenum, null,null, rand) -1;
+           ChooseRule(rulenum, null,null, rand);
 
         }
         else if (transform.parent !=null && !transform.parent.CompareTag("Level")) {
@@ -77,20 +76,18 @@ public class SpawnObjects : MonoBehaviour
             
             if( transform.parent.transform.parent == null || transform.parent.transform.parent.transform.parent == null || transform.parent.transform.parent.transform.parent.transform.parent == null || transform.parent.transform.parent.transform.parent.transform.parent.transform.parent == null)  {
             
-                rand = ChooseRule(rulenum, parenttiles, null, rand) -1;       
+              ChooseRule(rulenum, parenttiles, null, rand);       
             }
             else {
                 GameObject parent5 = transform.parent.transform.parent.transform.parent.transform.parent.transform.parent.gameObject;
                 string parent5tiles = parent5.tag;
 
-                rand = ChooseRule(rulenum, parenttiles, parent5tiles, rand) -1;
+                ChooseRule(rulenum, parenttiles, parent5tiles, rand);
             }
         }
         
+        rand = Random.Range(0, tiles.Count);
 
-        if(rand < 0) { rand =0;}
-
-        Debug.Log( rand + "   " + tiles.Count);
         Instantiate(tiles[rand], transform.position, Quaternion.identity);
 
         string currenttiles = tiles[rand].tag;
@@ -111,32 +108,32 @@ public class SpawnObjects : MonoBehaviour
     }
 
 
-    int ChooseRule(int num , string parent, string parent5, int rand)
+    void ChooseRule(int num , string parent, string parent5, int rand)
     {
-        int finalrand =0;
+       // string chosenrule ="No Rule";
 
-        if(num == 1)
+        if(num > 0 && num < 5)
         {
-            finalrand = NoSameNeigbhours(parent, parent5, rand);
-        }
-       /* if(num == 2)
-        {
-            Rule2();
-        }
-        if(num == 3)
-        {
-            Rule3();
-        }
-        else { NoRule(); }*/
+            NoSameNeigbhours(parent, parent5, rand);
 
-        return finalrand;
+            //chosenrule = "Tiles can't be of the type of their neighbours";
+        }
+        if(num >= 5 && num < 9)
+        {
+            SameNeigbhourschanceincrease(parent, parent5, rand);
+
+           // chosenrule = "More chance for tile to be of the same type of their neighbours";
+        }
+        if(num >= 9 && num < 13)
+        {
+            AllBlock(rand);
+           // chosenrule = "Tiles who can have walls will alays spawn a wall";
+        }
+        else {}  
     }
 
 
-    int  NoSameNeigbhours(string parent, string parent5, int rand) {
-
-        int lentghRemove = 0;
-        int newrand = rand;
+    void NoSameNeigbhours(string parent, string parent5, int rand) {
 
         if( parent != null && parent5 != null){
             for(int x = 0; x < tiles.Count; x++)
@@ -145,17 +142,6 @@ public class SpawnObjects : MonoBehaviour
                 {
                     tiles.Remove(tiles[x]);
                     x--;
-                    lentghRemove++;
-
-                }
-            }
-            if( rand > tiles.Count)
-            {
-                newrand = tiles.Count - lentghRemove;
-
-                if( newrand < 0)
-                {
-                     newrand = 0;
                 }
             }
         }
@@ -166,24 +152,54 @@ public class SpawnObjects : MonoBehaviour
                 {
                     tiles.Remove(tiles[x]);
                     x--;
-                    lentghRemove++;
-
-                }
-            }
-            if( rand > tiles.Count)
-            {
-                newrand = tiles.Count - lentghRemove;
-
-                if( newrand < 0)
-                {
-                     newrand = 0;
                 }
             }
         }
         else {}
+    }
 
-      //  Debug.Log(newrand + "   " + lentghRemove + "   "+ tiles.Count);
-        return newrand;
-        
+    void SameNeigbhourschanceincrease(string parent, string parent5, int rand)
+    {
+        int currenttilescount = tiles.Count;
+
+        for(int y = 0; y < 3; y++) {
+
+            if( parent != null && parent5 != null){
+                for(int x = 0; x < currenttilescount; x++)
+                {
+                    if (tiles[x].CompareTag(parent) || tiles[x].CompareTag(parent5))
+                    {
+                        tiles.Add(tiles[x]);
+                    }
+                } 
+                
+            }
+            else if( parent != null && parent5 == null){
+                for(int x = 0; x < currenttilescount; x++)
+                {
+                    if (tiles[x].CompareTag(parent))
+                    {
+                        tiles.Add(tiles[x]);
+                    }
+                }
+                
+                
+            }
+            else {}    
+        }
+    }
+
+    void  AllBlock(int rand) {
+
+        if(gameObject.layer == 7) {
+            for(int x = 0; x < tiles.Count; x++)                
+            {
+                if(tiles[x].layer == 6)
+                {
+                    tiles.Remove(tiles[x]);
+                    x--;;                                      
+                }
+            }
+        }
     }
 }
